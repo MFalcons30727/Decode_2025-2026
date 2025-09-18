8082aeba204245bb2b9bdbfdc029b5ff8987145c
MFalcons30727 <mfalcons30727@gmail.com>
2025-09-18 16:13:06 -0500

    FTCfile
    I made a complex code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCfile.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+
+public class FTCfile extends LinearOpMode {
+    @Override
+    public void runOpMode{} throws InterruptedException {
+
+    }
+}
