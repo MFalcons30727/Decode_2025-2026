154a58991b47d58c51c0c15dd1e6482f778d475e
MFalcons30727 <mfalcons30727@gmail.com>
2025-09-25 16:32:21 -0500

    Update AustinMecanumDrive.java
    🐉

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AustinMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AustinMecanumDrive.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
@@ -19,6 +20,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
         private DcMotor frontRightDrive = null;
         private DcMotor backRightDrive = null;
 
+        private DcMotor flywheel = null;
+
         @Override
         public void runOpMode() {
 
@@ -26,12 +29,13 @@ import com.qualcomm.robotcore.util.ElapsedTime;
         backLeftDrive = hardwareMap.get(DcMotor.class, "leftRear");
         frontRightDrive = hardwareMap.get(DcMotor.class, "rightFront");
         backRightDrive = hardwareMap.get(DcMotor.class, "rightRear");
-
+        flywheel = hardwareMap.get(DcMotor.class,"" );
 
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        flywheel.setDirection(DcMotor.Direction.FORWARD);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -44,6 +48,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
             double x = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
 
+
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
@@ -54,6 +59,12 @@ import com.qualcomm.robotcore.util.ElapsedTime;
             backLeftDrive.setPower(backLeftPower);
             frontRightDrive.setPower(frontRightPower);
             backRightDrive.setPower(backRightPower);
+            if(gamepad1.yWasPressed()){
+                flywheel.setPower(1);
+            }
+            else{
+                flywheel.setPower(0);
+            }
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
