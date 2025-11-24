2d76e57385ec82b68f12ff94dc38423bd39f05c6
austincommon-commits <austin.common@stafalcons.org>
2025-11-24 17:35:06 -0600

    did some tuning
    need some more tuning, ended off of the video with 26:00.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -16,13 +16,13 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(5.1) //has to be in kilograms, this is just a placeholder
+            .mass(8.709) //has to be in kilograms, this is just a placeholder
             // the placeholders I have right now are for the tuning part of this
             // TODO need to get values from telemetry by running each under automatic
-            .forwardZeroPowerAcceleration(1)
-            .lateralZeroPowerAcceleration(1)
+            .forwardZeroPowerAcceleration(-32.62296532427744)
+            .lateralZeroPowerAcceleration(-55.017228794291604)
             // the PIDF needs to be tuned accordingly, these are also placeholders
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.06, 0, 0.0001, 0.025))
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.08, 0, 0.001, 0.02))
             .headingPIDFCoefficients(new PIDFCoefficients(0.71, 0, 0.002, 0.025))
             .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.6, 0.0, 0.0001, 0.6, 0.025))
             //centripetal scaling is for curves, doesn't matter as much.
@@ -39,17 +39,17 @@ public class Constants {
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             // TODO this is the same as the top, 1 is just acting as a placeholder
-            .xVelocity(1)
-            .yVelocity(1);
+            .xVelocity(65.27527047705462)
+            .yVelocity(53.946332796352124);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-5) //TODO put in correct offsets we already measured
-            .strafePodX(0.5)
-            .distanceUnit(DistanceUnit.INCH)
+            .forwardPodY(8.8)
+            .strafePodX(25.5)
+            .distanceUnit(DistanceUnit.CM)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
