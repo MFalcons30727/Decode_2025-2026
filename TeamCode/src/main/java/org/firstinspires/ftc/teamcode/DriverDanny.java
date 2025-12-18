51c0e0e808c2c8ae3e29be953f5c84632a03a65a
abigailfussell-afk <abigail.fussell@stafalcons.org>
2025-12-17 18:36:22 -0600

    :)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverDanny.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+public class DriverDanny {
+    public static class Poses {
+        // put any poses we would ever need to go to in this list
+        public static final Pose RED_FAR_START_POSE = new Pose(96, 8, Math.toRadians(90));
+        public static final Pose BLUE_FAR_START_POSE = new Pose(49, 8, Math.toRadians(90));
+        public static final Pose RED_BACKUP_START_POSE = new Pose(122, 125, Math.toRadians(40));
+        public static final Pose BLUE_BACKUP_START_POSE = new Pose(22, 125, Math.toRadians(130));
+        public static final Pose RED_NEAR_SHOOTING_POSE = new Pose (96, 96, Math.toRadians(40));
+        public static final Pose BLUE_NEAR_SHOOTING_POSE = new Pose (49, 96, Math.toRadians(130));
+        public static final Pose RED_FAR_SHOOTING_POSE = new Pose (80, 16, Math.toRadians(65));
+        public static final Pose BLUE_FAR_SHOOTING_POSE = new Pose (62, 14, Math.toRadians(110));
+        public static final Pose PARK_RED_GATE_POSE = new Pose (120,70, Math.toRadians(180));
+        public static final Pose PARK_BLUE_GATE_POSE = new Pose (28,70, Math.toRadians(0));
+        public static final Pose RED_GOAL_POSE = new Pose(144, 144, Math.toRadians(225));
+        public static final Pose BLUE_GOAL_POSE = new Pose(0, 144, Math.toRadians(315));
+    }
+
+    public enum Team {
+        RED,
+        BLUE
+    }
+    private Follower follower; // part of the Pedro Pathing package, follows the path
+    private Telemetry telemetry;
+    private PathChain currentPath; // the current or most recent path we've built for the robot
+    private Team currentTeam;
+
+    public DriverDanny(HardwareMap hardwareMap, Telemetry telemetryFromOpMode, Pose startingPose, Team team) {
+        // this is our constructor that gets called like this from our autos:  driver = new DriverDanny(hardwareMap, telemetry, DriverDanny.Poses.RED_FAR_START_POSE);
+        // think of this like our "init" but for the DriverDanny specifically
+        follower = Constants.createFollower(hardwareMap);  // TODO: need to retune Pedro Pathing constants with the new bot
+        telemetry = telemetryFromOpMode;
+        follower.setStartingPose(startingPose);
+        currentTeam = team;
+    }
+
+    public void update() { // THIS MUST ALWAYS GO IN YOUR OPMODE LOOP EVERY CALL
+        follower.update(); // this will just update the Pedro Pathing following but can add additional steps if we need to later
+
+        Pose currentPose = getPose();
+        telemetry.addData("CurrentXPos", currentPose.getX());
+        telemetry.addData("CurrentYPos", currentPose.getY());
+        telemetry.addData("CurrentHeading", currentPose.getHeading());
+        telemetry.addData("CurrentDistanceFromGoal", getCurrentDistanceFromGoal());
+    }
+
+    public Pose getPose() {
+        return follower.getPose(); // this will just return the current pose from Pedro Pathing but can add additional steps if we need to later
+    }
+
+    /**
+     * See if the robot is still in the process of moving to a new pose
+     *
+     * @return whether or not the robot is still moving to the new pose
+     */
+    public boolean isBusy() {
+        return follower.isBusy(); // checks to see if PedroPathing is still busy and returns that result
+    }
+
+    /**
+     * Moves the robot from our current position to the new pose we pass as a parameter here
+     *
+     * @param newPose the new pose to move to
+     * @param holdEnd whether or not to have PedroPathing hold the robot at the newPose when done
+     */
+    public void moveToPose(Pose newPose, boolean holdEnd) {
+        currentPath = follower.pathBuilder()
+                .addPath(new BezierLine(getPose(), newPose))
+                .setLinearHeadingInterpolation(getPose().getHeading(), newPose.getHeading())
+                .build();
+
+        follower.followPath(currentPath, holdEnd); // start the robot moving towards the new pose immediately
+    }
+
+    public double getCurrentDistanceFromGoal() {
+        if (currentTeam == Team.RED) {
+            return getPose().distanceFrom(Poses.RED_GOAL_POSE);
+        } else {
+            return getPose().distanceFrom(Poses.BLUE_GOAL_POSE);
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterMcGavin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterMcGavin.java
@@ -1,11 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
+import com.arcrobotics.ftclib.util.InterpLUT;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -20,37 +20,57 @@ public class ShooterMcGavin {
         START_SPIN_UP, // start spinning up the shooter to the SHOOTER_TARGET_VELOCITY
         WAIT_FOR_TARGET_VELOCITY, // wait until we're at the SHOOTER_TARGET_VELOCITY before feeding
         START_FEEDING, // start indexers to feed one artifact
-        STOP_FEEDING, // stop indexers so the shooter has time to get back to the SHOOTER_TARGET_VELOCITY
-        FLYWHEEL_RECOVERY
+        STOP_FEEDING // stop indexers so the shooter has time to get back to the SHOOTER_TARGET_VELOCITY
     }
 
-    private static double SHOOTER_TARGET_VELOCITY = 1200; // the velocity we want our shooter to be set to.
+    private double SHOOTER_TARGET_VELOCITY = 1100; // the velocity we want our shooter to be set to
+    private double HOOD_SERVO_POSITION = 0; // the servo position of the adjustable hood
     private static final double SHOOTER_ACCEPTABLE_VELOCITY_ERROR = 30; // in case the shooter motor isn't able to reach that exact velocity, allow it to still shoot when being this close to the target velocity
-    private static final double TIME_TO_FEED_IN_MILLISECONDS = 300; // this is how long it takes our indexers to feed one artifact through (the time between starting and stopping the indexers)
-    private static final double FLYWHEEL_RECOVERY_TIME_IN_MILLISECONDS = 300; // if using USE_RAPID_FIRE, this delay will be skipped
-    private static final double FEEDER_POWER = 1.0; // the power we send to the indexer motor to feed
-    private static final double STEP_TIMEOUT_IN_MILLISECONDS = 5000;
-    private static final boolean USE_RAPID_FIRE = true;
+    private static final double TIME_TO_FEED_IN_MILLISECONDS = 1000; // this is how long it takes our indexers to feed one artifact through (the time between starting and stopping the indexers)
+    private static final double FEEDER_POWER = 1; // the power we send to the indexer servos to feed
+    private static final double STEP_TIMEOUT_IN_MILLISECONDS = 2000;
     private final DcMotorEx shootMotor;
     private final DcMotor indexer;
+    private final Servo hoodServo;
+    private Telemetry telemetry;
     private ElapsedTime shootStateTimer; // tried to use the Pedro Pathing timer first but it didn't allow for milliseconds, only seconds
     private ShootingState currentShootingState; // keeping track of the current step we're on in our shooting state machine
     private int shotsFired; // keeps track of how many artifacts we've attempted to shoot (between 0 and 3)
+    private InterpLUT velocityLUT, hoodServoPositionLUT;
 
-    public ShooterMcGavin(HardwareMap hardwareMap, Telemetry telemetry) { // this is our constructor that gets called like this from our autos:  shooter = new Shooter(hardwareMap);
+    public ShooterMcGavin(HardwareMap hardwareMap, Telemetry telemetryFromOpMode) { // this is our constructor that gets called like this from our autos:  shooter = new Shooter(hardwareMap);
         // think of this like our "init" but for the Shooter specifically
         shootMotor = hardwareMap.get(DcMotorEx.class, "shooter");
         indexer = hardwareMap.get(DcMotor.class, "indexer");
+        hoodServo = hardwareMap.get(Servo.class, "hood");
+        telemetry = telemetryFromOpMode;
         shootStateTimer = new ElapsedTime();
         currentShootingState = ShootingState.OFF;
         shotsFired = 0;
+        velocityLUT = new InterpLUT();
+        hoodServoPositionLUT = new InterpLUT();
+        buildLUTS();
 
         shootMotor.setDirection(DcMotorEx.Direction.REVERSE);
 
         indexer.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(190, 0, 0, 12); // If not reaching target velocity, increase F.  If not recovering fast enough, increase P
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(181,0,0,12); // TODO: need to retune these with the new bot
         shootMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+    }
+
+    private void buildLUTS() {
+        // TODO: need to measure certain distances and determine what velocity and hood servo positions are best
+        // the first parameter of both velocityLUT and hoodServoPositionLUT below is the measured distance from goal (use tape measure from goal to front of robot)
+        // on velocityLUT, the second parameter is what velocity we need to set the flywheel to for it to make it
+        // on hoodServoPositionLUT, the second parameter is what position we need to set the hood servo to for the best angle
+        velocityLUT.add(36, 1100);
+        velocityLUT.add(60, 1400);
+        velocityLUT.createLUT();
+
+        hoodServoPositionLUT.add(36, 0.25);
+        hoodServoPositionLUT.add(60, 0.75);
+        hoodServoPositionLUT.createLUT();
     }
 
     private void setShootingState(ShootingState newState) {
@@ -68,66 +88,58 @@ public class ShooterMcGavin {
     }
 
     public void update() { // THIS MUST ALWAYS GO IN YOUR OPMODE LOOP EVERY CALL
-        telemetry.addData("ShooterState", currentShootingState.toString());
-        telemetry.addData("ShotsFired", shotsFired);
-        telemetry.addData("ShooterVelocity", shootMotor.getVelocity());
-        telemetry.addData("ShooterStepTimerMS", shootStateTimer.milliseconds());
-
         switch (currentShootingState) {
             case START_SPIN_UP: // start the shooter
                 shotsFired = 0;
                 shootMotor.setVelocity(SHOOTER_TARGET_VELOCITY);
+                hoodServo.setPosition(HOOD_SERVO_POSITION);
                 setShootingState(ShootingState.WAIT_FOR_TARGET_VELOCITY);
                 break;
             case WAIT_FOR_TARGET_VELOCITY: // wait until we're close to the target velocity for the shooter
-                if (Math.abs(shootMotor.getVelocity() - SHOOTER_TARGET_VELOCITY) < SHOOTER_ACCEPTABLE_VELOCITY_ERROR
+                if ((Math.abs(shootMotor.getVelocity() - SHOOTER_TARGET_VELOCITY) < SHOOTER_ACCEPTABLE_VELOCITY_ERROR && hoodServo.getPosition() == HOOD_SERVO_POSITION)
                         || shootStateTimer.milliseconds() > STEP_TIMEOUT_IN_MILLISECONDS) { // this makes sure the auto doesn't fail completely if it's not able to ever reach target velocity
                     setShootingState(ShootingState.START_FEEDING);
                 }
                 break;
-            case START_FEEDING: // turn on the indexer to feed the next artifact
+            case START_FEEDING: // turn on the indexers to feed the next artifact
                 indexer.setPower(FEEDER_POWER);
 
-                // for future, might be cool to check the velocity change on the flywheel to detect if a shot occurred instead of timing
-
-                // if using USE_RAPID_FIRE = true, the total time the indexer will run is 3 x TIME_TO_FEED_IN_MILLISECONDS
-                if (shootStateTimer.milliseconds() > TIME_TO_FEED_IN_MILLISECONDS) { // wait for the indexer to finish feeding the artifact.
+                if (shootStateTimer.milliseconds() > TIME_TO_FEED_IN_MILLISECONDS) { // wait for the indexer to finish feeding the artifact
                     shotsFired++;
                     setShootingState(ShootingState.STOP_FEEDING);
                 }
                 break;
             case STOP_FEEDING:
-                if (!USE_RAPID_FIRE) {
-                    indexer.setPower(0);
-                }
+                indexer.setPower(0);
 
                 if (shotsFired >= 3) { // after 3 shot attempts, power off shooter
                     shotsFired = 0;
                     setShootingState(ShootingState.OFF);
                 }
                 else {
-                    setShootingState(ShootingState.FLYWHEEL_RECOVERY); // if more shots to fire, wait for the shooter to reach target velocity again
-                }
-                break;
-            case FLYWHEEL_RECOVERY:
-                if (shootStateTimer.milliseconds() > FLYWHEEL_RECOVERY_TIME_IN_MILLISECONDS || USE_RAPID_FIRE) {
-                    setShootingState(ShootingState.WAIT_FOR_TARGET_VELOCITY);
+                    setShootingState(ShootingState.WAIT_FOR_TARGET_VELOCITY); // if more shots to fire, wait for the shooter to reach target velocity again
                 }
                 break;
             case OFF:
                 shootMotor.setVelocity(0);
                 indexer.setPower(0);
-                SHOOTER_TARGET_VELOCITY = 1200;
                 break;
         }
+
+        telemetry.addData("FlywheelVelocity", shootMotor.getVelocity());
+        telemetry.addData("HoodServoPosition", hoodServo.getPosition());
     }
 
-    public void startShooting() { // this is what will be called by our main auto code to shoot 3 artifacts automatically
+    public void startShootingFromDistance(double distanceFromGoalInInches) { // if this version of startShooting is called with no arguments, use the LUTs to determine velocity and hood servo position
+        // maybe with LEDs, if it's a distance we can shoot from, turn LED green.  otherwise turn it red.
+        // may need to add some error handling here since InterpLUT will throw exceptions if we ask for a distance that is outside of the min/max values we measured
+        SHOOTER_TARGET_VELOCITY = velocityLUT.get(distanceFromGoalInInches);
+        HOOD_SERVO_POSITION = hoodServoPositionLUT.get(distanceFromGoalInInches);
         setShootingState(ShootingState.START_SPIN_UP);
     }
-
-    public void startShooting(double targetVelocity) { // this is what will be called by our main auto code to shoot 3 artifacts automatically
+    public void startShootingAtVelocity(double targetVelocity) {
         SHOOTER_TARGET_VELOCITY = targetVelocity;
+        HOOD_SERVO_POSITION = 0; // retract hood all the way
         setShootingState(ShootingState.START_SPIN_UP);
     }
 }
