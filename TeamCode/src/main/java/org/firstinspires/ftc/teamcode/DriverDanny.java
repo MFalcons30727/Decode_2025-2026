435a74a91c421e01035702f5d561ebcf77644cb2
abigailfussell-afk <abigail.fussell@stafalcons.org>
2026-02-02 19:55:27 -0600

    moved over fixes from other branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverDanny.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverDanny.java
@@ -151,9 +151,7 @@ public class DriverDanny {
         this.updateLimeLight(); // should update our limelight every loop
 
         // if the robot has changed position, reset the timer so we can track how long we've been idle
-        if (lastKnownPose != null && (lastKnownPose.getX() != this.getPose().getX()
-                || lastKnownPose.getY() != this.getPose().getY()))
-        {
+        if (lastKnownPose != null && lastKnownPose.distanceFrom(this.getPose()) > 0.5) {
             idleTimer.reset();
         }
 
@@ -192,9 +190,11 @@ public class DriverDanny {
 
                 if (currentAlliance == Alliance.BLUE && tagID == 20) {
                     limelightGoalHeadingError = fr.getTargetXDegrees();
+                    break;
                 }
                 else if (currentAlliance == Alliance.RED && tagID == 24) {
                     limelightGoalHeadingError = fr.getTargetXDegrees();
+                    break;
                 } else {
                     limelightGoalHeadingError = -999;
                 }
