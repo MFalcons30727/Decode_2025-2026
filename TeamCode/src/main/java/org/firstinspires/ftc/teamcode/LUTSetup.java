1107b2f0f71420dd0a74056341e60788d47908fb
abigailfussell-afk <abigail.fussell@stafalcons.org>
2026-02-21 18:41:28 -0600

    oops - missed one file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LUTSetup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LUTSetup.java
@@ -16,7 +16,7 @@ public class LUTSetup extends OpMode {
     @Override
     public void init() {
         DriverDanny.Alliance startingAlliance = DriverDanny.Alliance.BLUE;
-        Pose startingPose = DriverDanny.Poses.BFA_START_POSE;
+        Pose startingPose = DriverDanny.Poses.BLUE_FAR_START_POSE;
 
         driver = new DriverDanny(hardwareMap,
                 telemetry,
@@ -45,12 +45,7 @@ public class LUTSetup extends OpMode {
         }
 
         if (gamepad2.right_bumper) {
-            rotate = driver.getHeadingErrorForAutoAimLimelight();
-
-            //if not able to find april tag revert to Trig based aiming
-            if (rotate == -1) {
-                rotate = driver.getHeadingErrorForAutoAimTrig();
-            }
+            rotate = driver.getHeadingErrorForAutoAimTrig();
         }
 
         if (gamepad2.left_trigger > 0.25 && !shooter.isShooting()) {
